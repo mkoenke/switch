24ee1f46651284d448bcdfe9c322693f35dc4b09
mkoenke <mkoenke@sas.upenn.edu>
2020-11-30 20:59:47 -0500

    index.html and event listeners on login and form

--- src/index.js
+++ src/index.js
@@ -6,3 +6,30 @@ fetch(`${URL}/users`)
     .then(users => {
         console.log(users)
     })
+//-----------------------------------------------------------
+
+//dom elements
+const navBar = document.querySelector(".topnav")
+const loginDiv = document.querySelector("#login")
+// const cancelBtn = modal.querySelector(".cancelBtn")
+
+
+loginDiv.addEventListener("click", getLoginForm)
+//login form 
+function getLoginForm(event){
+    if (event.target.id === "login"){
+        // console.log("I was clicked")
+        document.getElementById('modal').style.display='block'
+    }
+}
+const modal = document.getElementById('modal')
+
+// When the user clicks anywhere outside of the modal, close it
+document.addEventListener("click", outsideFormClick)
+
+function outsideFormClick(event){
+    // console.log(event.target)
+    if (event.target == modal) {
+        modal.style.display = "none"
+      }
+}
