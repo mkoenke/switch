dd39264d88377d403e8c7a5b74d8fb43b2026be9
EpicDoomSir <eitands@gmail.com>
2020-12-01 16:30:18 -0500

    fine tuned timer

--- src/timer.js
+++ src/timer.js
@@ -6,19 +6,31 @@ const renderTimerForGame = (gameInfo) => {
 
 const startTimer = () => {
     const interval = setInterval(() => {
-        timer.textContent = parseInt(timer.textContent) - 1
-        if (parseInt(timer.textContent) === 0){
+        let decimal = 4
+
+        timer.textContent = parseFloat(timer.textContent) - 0.1
+
+        if (parseInt(timer.textContent) >= 100) {
+            decimal = 4
+        } else if (100 > parseInt(timer.textContent) && parseInt(timer.textContent) >= 10) {
+            decimal = 3
+        } else {
+            decimal = 2
+        }
+
+        timer.textContent = parseFloat(timer.textContent).toPrecision(decimal)
+        if (parseFloat(timer.textContent) === 0){
             clearInterval(interval)
         }
-    }, 1000)
+    }, 100)
 }
 
 
 const sampleGameInfo = {
     "id": 1,
     "title": "memory",
-    "time": 20
+    "time": 15
 }
 
 renderTimerForGame(sampleGameInfo)
-// startTimer()
+startTimer()
