e9cfcce29ff2cb0330b35dec345db15edf7e0c43
EpicDoomSir <eitands@gmail.com>
2020-12-02 15:37:45 -0500

    small tweaks

--- src/userProfile.js
+++ src/userProfile.js
@@ -7,10 +7,7 @@ const renderUserProfile = (userObj) => {
     username.textContent = `Username: ${userObj.username}`
     points.textContent = `Points: ${userObj.totalPoints}`
     avatar.src = userObj.avatar
-<<<<<<< HEAD
-=======
     console.log(userObj)
->>>>>>> 845c71cab72ebd2741771267762136d9aaf5a6d2
 }
 
 
