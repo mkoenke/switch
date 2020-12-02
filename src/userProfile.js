845c71cab72ebd2741771267762136d9aaf5a6d2
EpicDoomSir <eitands@gmail.com>
2020-12-02 15:12:38 -0500

    avatar shows up

--- src/userProfile.js
+++ src/userProfile.js
@@ -2,13 +2,13 @@ const profile = document.querySelector("#profile")
 console.log(profile)
 const username = profile.querySelector("#username")
 const points = profile.querySelector("#points")
-const avatar = profile.querySelectorAll("img")
+const avatar = profile.querySelector("img")
 
 const renderUserProfile = (userObj) => {
     username.textContent = `Username: ${userObj.username}`
     points.textContent = `Points: ${userObj.totalPoints}`
     avatar.src = userObj.avatar
-    // console.log(username)
+    console.log(userObj)
 }
 
 const sampleUser = {
