03b7a93c060cfe5a24767a693b77520becf4a2fd
mkoenke <mkoenke@sas.upenn.edu>
2020-12-02 16:50:00 -0500

    user deleted persisting

--- src/userProfile.js
+++ src/userProfile.js
@@ -3,12 +3,32 @@ const username = profile.querySelector("#username")
 const points = profile.querySelector("#points")
 const avatar = profile.querySelector("img")
 
+
 const renderUserProfile = (userObj) => {
     username.textContent = `Username: ${userObj.username}`
     points.textContent = `Points: ${userObj.totalPoints}`
     avatar.src = userObj.avatar
-    console.log(userObj)
+    // console.log(userObj)
+    const deleteButton = document.createElement("button")
+    deleteButton.textContent = "Delete User Profile"
+    profile.append(deleteButton)
+    deleteButton.addEventListener("click", deleteUser)
+
 }
 
-
+function deleteUser(){
+  
+fetch(`${URL}/users/${currentUser.id}`, {
+  method: 'DELETE' 
+})
+.then(response => response.json())
+.then(deletedUser => {
+  currentUser = null
+  ///GO TO HOME SCREEN
+  console.log('Deleted User:', deletedUser)
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
+}
   
